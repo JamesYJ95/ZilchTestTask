72b04263ec93680ae2722e006ce075d55cabc620
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-09-19 22:02:33 +0100

    Add TestRunner

--- src/test/java/com/jameszilchtask/runners/TestRunner.java
+++ src/test/java/com/jameszilchtask/runners/TestRunner.java
@@ -1,4 +1,16 @@
 package com.jameszilchtask.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.jameszilchtest.steps"},
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        monochrome = true
+)
+
 public class TestRunner {
 }
