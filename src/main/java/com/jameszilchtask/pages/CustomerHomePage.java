810fc7a33c00270abd7cf1ef07e3b518b671505e
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-09-19 22:00:23 +0100

    Add first pages

---
+++ src/main/java/com/jameszilchtask/pages/CustomerHomePage.java
@@ -0,0 +1,22 @@
+package com.jameszilchtask.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CustomerHomePage extends BasePage {
+
+    @FindBy(className = "customer-toggle")
+    private WebElement customerToggle;
+
+    @FindBy(partialLinkText = "Sign in")
+    private WebElement signInButton;
+
+    public CustomerHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickSignIn() {
+        signInButton.click();
+    }
+}
